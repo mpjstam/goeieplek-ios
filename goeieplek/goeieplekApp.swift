fb83fbfd2337bf03dc44898a6abb99dfacb90802
Michiel Stam <info@michielstam.nl>
2026-07-29 22:21:03 +0200

    Initial Commit

---
+++ goeieplek/goeieplekApp.swift
@@ -0,0 +1,17 @@
+//
+//  goeieplekApp.swift
+//  goeieplek
+//
+//  Created by Michiel Stam on 29/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct goeieplekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
